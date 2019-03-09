b91c5f3bcfe530f3000e5ada39ea4dc156f3ba3c
Jean Champémont <git@jeanchampemont.com>
2019-03-09 10:01:38 +0100

    First commit

---
+++ src/main/java/com/jeanchampemont/keepasscloud/KeepassCloudApplication.java
@@ -0,0 +1,13 @@
+package com.jeanchampemont.keepasscloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepassCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepassCloudApplication.class, args);
+	}
+
+}
